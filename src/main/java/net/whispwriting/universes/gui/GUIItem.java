612391b1d2a3dea940b5a8510638ea897c135026
DSlyDog <drew.sly@verizon.net>
2025-05-22 05:00:20 -0400

    Updates to the GUIItem class

--- src/main/java/net/whispwriting/universes/gui/GUIItem.java
+++ src/main/java/net/whispwriting/universes/gui/GUIItem.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 
-public class GUIItem {
+public abstract class GUIItem {
 
     private String displayName;
     private int amount = 1;
